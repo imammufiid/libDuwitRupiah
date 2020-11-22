08c58874e28050e5f0801dfe153c6628dc6086cf
imufiid <imufiid98@gmail.com>
2020-11-22 15:49:04 +0700

    testing in app

--- app/src/main/java/com/mufiid/myfirstlibrary/MainActivity.kt
+++ app/src/main/java/com/mufiid/myfirstlibrary/MainActivity.kt
@@ -2,10 +2,20 @@ package com.mufiid.myfirstlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.mufiid.mylibrary.DuwitRupiah
 
 class MainActivity : AppCompatActivity() {
+    private var duwitRupiah: DuwitRupiah? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        duwitRupiah = DuwitRupiah()
+        val result = duwitRupiah?.generate("2500000")
+
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
     }
 }
